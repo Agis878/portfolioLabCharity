5ad5895e5148512fafd34d71bcb2a038b92a3683
Agis878 <a.lukasik@op.pl>
2023-11-13 15:05:44 +0100

    Update Institutions Entity

--- src/main/java/pl/coderslab/charity/model/Institution.java
+++ src/main/java/pl/coderslab/charity/model/Institution.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,6 +23,9 @@ public class Institution {
     private String name;
 
     private String description;
+
+    @OneToMany(mappedBy = "institution")
+    private List<Donation> donations;
 
     @Override
     public String toString() {
