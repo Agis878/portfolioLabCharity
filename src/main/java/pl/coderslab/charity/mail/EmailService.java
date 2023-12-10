f66c03ebf585bcda2d2cc304061977db185dfa69
Agis878 <a.lukasik@op.pl>
2023-12-10 14:03:01 +0100

    Organize project folders

--- src/main/java/pl/coderslab/charity/mail/EmailService.java
+++
@@ -1,8 +0,0 @@
-package pl.coderslab.charity.mail;
-
-
-public interface EmailService {
-    void sendMessageFromContactForm(Feedback feedback);
-
-    void sendActivationEmail(String username, String activationCode);
-}
